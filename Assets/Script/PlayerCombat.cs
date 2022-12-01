ad432feb84ece929b87562913809ddef15e716f4
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-12-01 17:23:04 +0000

    Initial commit

---
+++ Assets/Script/PlayerCombat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackpoint;
+    public float attackrange = 0.5f;
+    public LayerMask enemylayer;
+    public UnityEngine.Experimental.Rendering.Universal.Light2D FOV;
+    public float attackRate = 1f;
+    float nextAttackTime = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+        Collider2D[] hitenemy = Physics2D.OverlapCircleAll(attackpoint.position,attackrange,enemylayer);
+        foreach(Collider2D enemy in hitenemy)
+        {
+            Destroy(enemy.gameObject);
+            FOV.pointLightOuterRadius += 0.5f;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(attackpoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackpoint.position, attackrange);
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    Rigidbody2D rb2d;
+    bool isGrounded;
+
+    [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
+    Transform groundCheckL;
+    [SerializeField]
+    Transform groundCheckR;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+    }
+
+    private void FixedUpdate()
+    {
+        if(Physics2D.Linecast(transform.position,groundCheck.position, 1 << LayerMask.NameToLayer("/Ground")) || Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Ground")) || Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Ground")) ||
+           Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Platform")) || Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Platform")) || Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Platform")))
+        {
+            isGrounded = true;
+            animator.SetBool("Ground", true);
+        }
+        else
+        {
+            isGrounded = false;
+            animator.SetBool("Ground", false);
+        }
+        if(Input.GetKey("d"))
+        {
+            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+            rb2d.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+            rb2d.transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);         
+        }
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 4);
+        }
+    }
+
+}
